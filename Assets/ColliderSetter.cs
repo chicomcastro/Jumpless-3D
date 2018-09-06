4c9e8f0e3252df264af99b8b5f9ffe2bf2ecd367
chicomcastro <Franciscommcastro@gmail.com>
2018-09-05 21:40:11 -0300

    Correction on colliders and CollideSetter scripting

---
+++ Assets/ColliderSetter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ColliderSetter : MonoBehaviour
+{
+    public new string name;
+
+    [ContextMenu("Set colliders to trigger")]
+    public void SetCollider(string name)
+    {
+        Collider[] colliders = GameObject.FindObjectsOfType<Collider>();
+
+        foreach (Collider col in colliders)
+        {
+            if (col.gameObject.name.ToLower().Contains(name.ToLower()))
+                col.isTrigger = true;
+        }
+    }
+}
---
+++ Assets/EditorColliderSetter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ColliderSetter))]
+public class EditorColliderSetter : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ColliderSetter myScript = (ColliderSetter)target;
+        GUILayout.Space(10);
+
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Set colliders to trigger"))
+        {
+            myScript.SetCollider(myScript.name);
+        }
+
+        GUILayout.EndHorizontal();
+    }
+
+}
